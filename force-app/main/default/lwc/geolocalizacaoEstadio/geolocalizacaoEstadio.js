f5b7521907529b53148426125ffb584de30177a4
Maxwell Jranatu Swaggerson, XCV <tutuswagger@gmail.com>
2026-08-30 19:55:40 -0300

    acho q fiz merda rapaziada

---
+++ force-app/main/default/lwc/geolocalizacaoEstadio/geolocalizacaoEstadio.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+
+import LATITUDE from '@salesforce/schema/Estadio__c.Latitude__c';
+import LONGITUDE from '@salesforce/schema/Estadio__c.Longitude__c';
+import NAME from '@salesforce/schema/Estadio__c.Name';
+
+const fields = [LATITUDE, LONGITUDE, NAME];
+
+export default class GeolocalizacaoEstadio extends LightningElement {
+
+    @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields: fields})
+    estadio;
+
+    get mapMarkers(){
+        const latitude = getFieldValue(this.estadio.data, LATITUDE);
+
+        const longitude = getFieldValue(this.estadio.data, LONGITUDE);
+
+        const name = getFieldValue(this.estadio.data, NAME);
+
+        if(!latitude || !longitude){
+            return [];
+        }
+
+        return [
+            {
+                location: {
+                    latitude: latitude,
+                    longitude: longitude
+                },
+                title: name
+            }
+        ];
+        
+    }
+}
